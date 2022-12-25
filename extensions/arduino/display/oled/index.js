7635c6b58da1a8899b6c75d10a2387df8c11de00
ygrobot <58776239+haib78@users.noreply.github.com>
2022-12-25 15:30:38 +0800

    12-25-3

--- extensions/arduino/display/oled/index.js
+++ extensions/arduino/display/oled/index.js
@@ -1,15 +1,10 @@
-const oled = formatMessage => ({
+const OLEDYG = formatMessage => ({
     name: 'OLED',
-    extensionId: 'oled',
-    version: '0.0.1',
-    supportDevice: ['arduinoUno', 'arduinoNano', 'arduinoLeonardo',
-        'arduinoMega2560', 'arduinoEsp32', 'arduinoEsp8266'],
-    author: 'ArthurZheng',
+    extensionId: 'OLEDYG',
+    //version: '0.0.1',
+    supportDevice: ['RoboTexline','ZQrobot201','ZQrobot202'],
+    //author: 'ArthurZheng',
     iconURL: `asset/oled.png`,
-    description: formatMessage({
-        id: 'oled.description',
-        default: 'I2C oled display based on SSD1306 drivers.'
-    }),
     featured: true,
     blocks: 'blocks.js',
     generator: 'generator.js',
@@ -17,8 +12,8 @@ const oled = formatMessage => ({
     msg: 'msg.js',
     library: 'lib',
     official: true,
-    tags: ['display'],
-    helpLink: 'https://wiki.openblock.cc'
+    tags: ['display']
+    //helpLink: 'https://openblockcc.gitee.io/wiki/main'
 });
 
-module.exports = oled;
+module.exports = OLEDYG;
